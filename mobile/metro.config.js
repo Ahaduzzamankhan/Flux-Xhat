0d057f597a554d072dbff61da1dfcbe7a4c10b77
Ahaduzzamankhan <bdgamer9191@gmail.comgit config --global user.name>
2026-05-07 14:40:00 +0600

    FIx : android build alpha v

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,5 +1,13 @@
+const path = require('path');
+
 /** @type {import('metro-config').MetroConfig} */
 module.exports = {
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(__dirname, 'node_modules'),
+    ],
+    resolverMainFields: ['react-native', 'browser', 'main'],
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
